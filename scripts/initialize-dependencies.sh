3f84a0b5acd6dbf3e0a2afe8f21cce40e42f0ddc
Olivier Laurent <olivier-xavier.laurent@ac-toulouse.fr>
2025-01-29 22:46:17 +0000

    Suite du cours vecteurs II

--- scripts/initialize-dependencies.sh
+++ scripts/initialize-dependencies.sh
@@ -8,44 +8,4 @@ sudo cp ./texmf/math-vh.sty /usr/local/texlive/2024/texmf-dist/tex/latex
 sudo texhash
 
 
-sudo tlmgr install ntheorem
-sudo tlmgr install pgf
-sudo tlmgr install tcolorbox
-sudo tlmgr install framed
-sudo tlmgr install etoolbox
-sudo tlmgr install varwidth
-sudo tlmgr install tkz-tab
-sudo tlmgr install proflycee
-sudo tlmgr install mathtools
-sudo tlmgr install xkeyval
-sudo tlmgr install xstring
-sudo tlmgr install xint
-sudo tlmgr install poormanlog
-sudo tlmgr install randomlist
-sudo tlmgr install simplekv
-sudo tlmgr install listofitems
-sudo tlmgr install tabularray
-sudo tlmgr install hologo
-sudo tlmgr install fancyvrb
-sudo tlmgr install xfrac
-sudo tlmgr install units
-sudo tlmgr install siunitx
-sudo tlmgr install fontawesome5
-sudo tlmgr install pdfcol
-sudo tlmgr install tikzfill
-sudo tlmgr install listings
-sudo tlmgr install listingsutf8
-sudo tlmgr install commalists-tools
-sudo tlmgr install modulus
-sudo tlmgr install cancel
-sudo tlmgr install nicematrix
-sudo tlmgr install datetime2
-sudo tlmgr install enumitem
-sudo tlmgr install qrcode
-sudo tlmgr install csquotes
-sudo tlmgr install lastpage
-sudo tlmgr install ninecolors
-sudo tlmgr install interval
-sudo tlmgr install esvect
-sudo tlmgr install mleftright
-sudo tlmgr install tabularx
+sudo tlmgr install ntheorem pgf tcolorbox framed etoolbox varwidth tkz-tab proflycee mathtools xkeyval xstring xint poormanlog randomlist simplekv listofitems tabularray hologo fancyvrb xfrac units siunitx fontawesome5 pdfcol tikzfill listings listingsutf8 commalists-tools modulus cancel nicematrix datetime2 enumitem qrcode csquotes lastpage ninecolors interval esvect mleftright tabularx
