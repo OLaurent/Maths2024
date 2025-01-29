75a2b62cc70bf860bed105da405cd8a784a474f5
Olivier Laurent <olivier-xavier.laurent@ac-toulouse.fr>
2025-01-29 22:53:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LaTeX-Maths-Template
+Template for editing LaTeX on Github Codespaces, mainly for typesetting mathematics.
+
+Includes basics packages for mathematics, such as ProfLycee, ...
+
+Uses an Ubuntu container, with Latex Workshop, and Copilot enabled for LaTeX.
---
+++ scripts/initialize-dependencies.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#sudo apt-get update
+#sudo apt-get install texlive-xetex
+#sudo apt-get install texlive-lang-french
+
+sudo cp ./texmf/math-vh.sty /usr/local/texlive/2024/texmf-dist/tex/latex
+sudo texhash
+
+
+sudo tlmgr install ntheorem
+sudo tlmgr install pgf
+sudo tlmgr install tcolorbox
+sudo tlmgr install framed
+sudo tlmgr install etoolbox
+sudo tlmgr install varwidth
+sudo tlmgr install tkz-tab
+sudo tlmgr install proflycee
+sudo tlmgr install mathtools
+sudo tlmgr install xkeyval
+sudo tlmgr install xstring
+sudo tlmgr install xint
+sudo tlmgr install poormanlog
+sudo tlmgr install randomlist
+sudo tlmgr install simplekv
+sudo tlmgr install listofitems
+sudo tlmgr install tabularray
+sudo tlmgr install hologo
+sudo tlmgr install fancyvrb
+sudo tlmgr install xfrac
+sudo tlmgr install units
+sudo tlmgr install siunitx
+sudo tlmgr install fontawesome5
+sudo tlmgr install pdfcol
+sudo tlmgr install tikzfill
+sudo tlmgr install listings
+sudo tlmgr install listingsutf8
+sudo tlmgr install commalists-tools
+sudo tlmgr install modulus
+sudo tlmgr install cancel
+sudo tlmgr install nicematrix
+sudo tlmgr install datetime2
+sudo tlmgr install enumitem
+sudo tlmgr install qrcode
+sudo tlmgr install csquotes
+sudo tlmgr install lastpage
+sudo tlmgr install ninecolors
+sudo tlmgr install interval
+sudo tlmgr install esvect
+sudo tlmgr install mleftright
+sudo tlmgr install tabularx
